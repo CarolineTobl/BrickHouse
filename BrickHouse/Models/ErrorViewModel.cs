5c66dcf21db65d600bb13e1a0ba81c424180a8cf
CarolineTobl <carolinetobler1222@gmail.com>
2024-04-09 11:01:58 -0600

    First Push

---
+++ BrickHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrickHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
