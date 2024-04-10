0053a6ad479c2817acd003b8d367fabca0825c55
CarolineTobl <carolinetobler1222@gmail.com>
2024-04-10 08:16:41 -0600

    Browse

---
+++ BrickHouse/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,14 @@
+﻿public class CheckoutViewModel
+{
+    public string SelectedBank { get; set; }
+    public IEnumerable<string> UniqueBanks { get; set; }
+
+    public string SelectedCardType { get; set; }
+    public IEnumerable<string> UniqueCardTypes { get; set; }
+
+    public string SelectedCountryOfTransaction { get; set; }
+    public IEnumerable<string> UniqueCountriesOfTransaction { get; set; }
+
+    public string SelectedShippingAddress { get; set; }
+    public IEnumerable<string> UniqueShippingAddresses { get; set; }
+}
