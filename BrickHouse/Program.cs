791ddbc0be0a375b03b03ef0988d5addb1589deb
CarolineTobl <carolinetobler1222@gmail.com>
2024-04-10 19:52:12 -0600

    CSP Header

--- BrickHouse/Program.cs
+++ BrickHouse/Program.cs
@@ -111,26 +111,25 @@ namespace BrickHouse
             // Redirect from HTTP
             app.UseHttpsRedirection();
 
-            // Add Content Security Policy
-            //app.Use(async (context, next) =>
-            //{
-            //    // CSP directive string
-            //    var csp = "default-src 'self'; " +
-            //              "style-src 'self' 'https://fonts.googleapis.com'; " + // Allows styles from Google Fonts
-            //              "font-src 'self' 'https://fonts.gstatic.com'; " + // Allows fonts to be loaded from Google Fonts
-            //              "script-src 'self' " + // Allows scripts from your domain
-            //              "'https://code.jquery.com' " + // Allows jQuery from code.jquery.com
-            //              "'https://cdnjs.cloudflare.com' " + // Allows Popper.js from cdnjs
-            //              "'https://stackpath.bootstrapcdn.com'; " + // Allows Bootstrap JavaScript from StackPath
-            //                                                         // Add other directives as needed
-            //              "";
+            //Add Content Security Policy
+            app.Use(async (context, next) =>
+            {
+                var csp = "default-src 'self'; " +
+                          "style-src 'self' 'https://fonts.googleapis.com' 'unsafe-inline'; " + // Allow styles from self, Google Fonts, and unsafe-inline for inline styles
+                          "font-src 'self' 'https://fonts.gstatic.com'; " + // Allow font files from self and Google Fonts
+                          "script-src 'self' " +
+                                     "'https://code.jquery.com' " + // jQuery CDN
+                                     "'https://cdnjs.cloudflare.com' " + // Cloudflare CDN (for Popper.js, etc.)
+                                     "'https://stackpath.bootstrapcdn.com' " + // Bootstrap CDN
+                                     "'unsafe-inline'; " + // unsafe-inline for inline scripts
+                          "connect-src 'self' ws: wss:; " + // Allow connections from self and WebSocket connections
+                                                            // ... any other directives you have
+                          "";
 
-            //    // Add the Content-Security-Policy header to the response
-            //    context.Response.Headers.Add("Content-Security-Policy", csp);
+                context.Response.Headers.Add("Content-Security-Policy", csp);
+                await next();
+            });
 
-            //    // Continue processing the request
-            //    await next();
-            //});
 
             // Enables static files in wwwroot folder
             app.UseStaticFiles();
